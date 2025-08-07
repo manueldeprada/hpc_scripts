c2e6ef1bd91a32d0fc1e9a16d0428b09a85ade81
Manuel de Prada Corral <6536835+manueldeprada@users.noreply.github.com>
2025-08-07 13:12:17 +0200

    Update torch_compile.sh

--- torch_compile.sh
+++ torch_compile.sh
@@ -1,13 +1,12 @@
 micromamba create -n py313_torchCompile python=3.13 -y
 micromamba activate py313_torchCompile
 
-export CUDA_HOME=$CONDA_PREFIX
-export CUDA_INC_PATH=$CONDA_PREFIX/targets/x86_64-linux/include:$CONDA_PREFIX/include
-
 micromamba install -c nvidia cuda-cudart-dev cuda-nvcc cudnn cuda-toolkit cmake ninja -y
 
 uv pip install -r requirements.txt
 
+export CUDA_HOME=$CONDA_PREFIX
+export CUDA_INC_PATH=$CONDA_PREFIX/targets/x86_64-linux/include:$CONDA_PREFIX/include
 export _GLIBCXX_USE_CXX11_ABI=1
 export TORCH_USE_CUDA_DSA=1
 
